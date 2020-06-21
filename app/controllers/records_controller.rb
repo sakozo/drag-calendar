32e03bdf301323ebb943ad2b22f0b4fb568da803
sakozo <youlianggit@gmail.com>
2020-06-21 15:18:08 +0900

    Update records_controller.rb

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,11 @@
 class RecordsController < ApplicationController
+  def create
+    record_params
+    redirect_to edit_calendar_path(1)
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:red,:green,:blue)
+  end
 end
