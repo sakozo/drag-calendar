77f8c200f6d5e4fd5eb092896de0a37db7fd0f3a
sakozo <youlianggit@gmail.com>
2020-06-21 12:01:41 +0900

    Update 20200621024521_create_records.rb

--- db/migrate/20200621024521_create_records.rb
+++ db/migrate/20200621024521_create_records.rb
@@ -1,7 +1,11 @@
 class CreateRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :records do |t|
-
+      #TODO:ユーザー機能未作成
+      #t.references :user,       null: false, foreign_key: true
+      t.date :date
+      t.integer :type
+      t.integer :time
       t.timestamps
     end
   end
